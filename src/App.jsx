a99c5bbf9c5aa8e9c3b88bcdb53ba584226bbd7c
jeongol <jungcode127@gmail.com>
2024-09-11 12:40:21 +0900

    실습 제출

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const App = () => {
+  const fetch = async () => {
+    const response = await axios.get();
+    return response;
+  };
+
+  return;
+  <div>
+    <h1>post</h1>
+  </div>;
+};
+
+export default App;
